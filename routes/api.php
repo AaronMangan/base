63a3d37918f9ff6a2d888bcc96c5ce621968dd62
Aaron Mangan <azza.mangan@gmail.com>
2024-11-29 22:23:02 +1000

    development

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,19 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\JWTAuthController;
+use App\Http\Middleware\JwtMiddleware;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::prefix('api')->group(function () {
+Route::post('register', [JWTAuthController::class, 'register']);
+Route::post('login', [JWTAuthController::class, 'login']);
+
+Route::middleware([JwtMiddleware::class])->group(function () {
+    Route::get('user', [JWTAuthController::class, 'getUser']);
+    Route::post('logout', [JWTAuthController::class, 'logout']);
+});
+// });
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
