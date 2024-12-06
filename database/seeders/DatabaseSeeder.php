541fcf4d849edec5c9be7155e0d397cb4a18a486
Aaron Mangan <azza.mangan@gmail.com>
2024-12-06 15:53:14 +1000

    development

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,90 +19,92 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Check that we have the necessary credentials.
-        if (!$this->environmentCredentials()) {
-            throw new \Exception('Please check your .env file has the right credentials, or remove this seeder');
-        }
+        Organisation::withoutEvents(function () {
+            // Check that we have the necessary credentials.
+            if (!$this->environmentCredentials()) {
+                throw new \Exception('Please check your .env file has the right credentials, or remove this seeder');
+            }
 
-        // Call other seeders.
-        $this->call([
-            StatusSeeder::class,        // Creates statuses to apply to the users.
-        ]);
+            // Call other seeders.
+            $this->call([
+                StatusSeeder::class,        // Creates statuses to apply to the users.
+            ]);
 
-        // Set the created models to the active status.
-        $activeStatus = Status::where('name', 'active')->first();
+            // Set the created models to the active status.
+            $activeStatus = Status::where('name', 'active')->first();
 
-        /**
-         * SUPER ADMIN
-         */
-        // Super Admin Organisation
-        $superOrg = Organisation::create([
-            'name' => env('SUPER_ORG_NAME'),
-            'address' => env('SUPER_ADDRESS', null),
-            'phone' => env('SUPER_PHONE', null),
-            'settings' => json_encode([])
-        ]);
+            /**
+             * SUPER ADMIN
+             */
+            // Super Admin Organisation
+            $superOrg = Organisation::create([
+                'name' => env('SUPER_ORG_NAME'),
+                'address' => env('SUPER_ADDRESS', null),
+                'phone' => env('SUPER_PHONE', null),
+                'settings' => json_encode([])
+            ]);
 
-        // Create a Super role
-        $roleSuper = Role::create(['name' => 'super']);
-        $permissionSuper = Permission::create(['guard_name' => 'web', 'name' => 'full permissions']);
-        $roleSuper->givePermissionTo($permissionSuper);
+            // Create a Super role
+            $roleSuper = Role::create(['name' => 'super']);
+            $permissionSuper = Permission::create(['guard_name' => 'web', 'name' => 'full permissions']);
+            $roleSuper->givePermissionTo($permissionSuper);
 
-        // Create a super admin user.
-        $superUser = User::create([
-            'name' => env('SUPER_NAME'),
-            'email' => env('SUPER_EMAIL'),
-            'password' => env('SUPER_PASSWORD'),
-            'organisation_id' => $superOrg->id,
-            'status_id' => $activeStatus->id ?? null,
-        ]);
-        // Assign that user the role.
-        $superUser->assignRole($roleSuper);
+            // Create a super admin user.
+            $superUser = User::create([
+                'name' => env('SUPER_NAME'),
+                'email' => env('SUPER_EMAIL'),
+                'password' => env('SUPER_PASSWORD'),
+                'organisation_id' => $superOrg->id,
+                'status_id' => $activeStatus->id ?? null,
+            ]);
+            // Assign that user the role.
+            $superUser->assignRole($roleSuper);
 
-        /**
-         * ADMIN USERS
-         */
-        // Regular Organisation
-        $adminOrg = Organisation::create([
-            'name' => env('ADMIN_ORG_NAME'),
-            'address' => env('ADMIN_ADDRESS', null),
-            'phone' => env('ADMIN_PHONE', null),
-            'settings' => json_encode([])
-        ]);
+            /**
+             * ADMIN USERS
+             */
+            // Regular Organisation
+            $adminOrg = Organisation::create([
+                'name' => env('ADMIN_ORG_NAME'),
+                'address' => env('ADMIN_ADDRESS', null),
+                'phone' => env('ADMIN_PHONE', null),
+                'settings' => json_encode([])
+            ]);
 
-        // Create an admin org and permissions
-        $roleAdmin = Role::create(['name' => 'admin']);
-        $permissionAdmin = Permission::create(['guard_name' => 'web', 'name' => 'admin']);
-        $roleAdmin->givePermissionTo($permissionAdmin);
+            // Create an admin org and permissions
+            $roleAdmin = Role::create(['name' => 'admin']);
+            $permissionAdmin = Permission::create(['guard_name' => 'web', 'name' => 'admin']);
+            $roleAdmin->givePermissionTo($permissionAdmin);
 
-        // Create an admin org and permissions
-        $roleUser = Role::create(['name' => 'user']);
-        $permissionUser = Permission::create(['guard_name' => 'web', 'name' => 'readonly']);
-        $roleUser->givePermissionTo($permissionAdmin);
+            // Create an admin org and permissions
+            $roleUser = Role::create(['name' => 'user']);
+            $permissionUser = Permission::create(['guard_name' => 'web', 'name' => 'readonly']);
+            $roleUser->givePermissionTo($permissionAdmin);
 
-        // Create an admin user.
-        $adminUser = User::create([
-            'name' => env('ADMIN_NAME'),
-            'email' => env('ADMIN_EMAIL'),
-            'password' => env('ADMIN_PASSWORD'),
-            'organisation_id' => $adminOrg->id,
-            'status_id' => $activeStatus->id ?? null,
-        ]);
+            // Create an admin user.
+            $adminUser = User::create([
+                'name' => env('ADMIN_NAME'),
+                'email' => env('ADMIN_EMAIL'),
+                'password' => env('ADMIN_PASSWORD'),
+                'organisation_id' => $adminOrg->id,
+                'status_id' => $activeStatus->id ?? null,
+            ]);
 
-        // Create a regular user.
-        $regularUser = User::create([
-            'name' => env('USER_NAME'),
-            'email' => env('USER_EMAIL'),
-            'password' => env('USER_PASSWORD'),
-            'organisation_id' => $adminOrg->id,
-            'status_id' => $activeStatus->id ?? null,
-        ]);
+            // Create a regular user.
+            $regularUser = User::create([
+                'name' => env('USER_NAME'),
+                'email' => env('USER_EMAIL'),
+                'password' => env('USER_PASSWORD'),
+                'organisation_id' => $adminOrg->id,
+                'status_id' => $activeStatus->id ?? null,
+            ]);
 
-        // Assign the role to that user.
-        $adminUser->assignRole($roleAdmin);
+            // Assign the role to that user.
+            $adminUser->assignRole($roleAdmin);
 
-        // Assign permissions to the regular user.
-        $regularUser->assignRole($roleUser);
+            // Assign permissions to the regular user.
+            $regularUser->assignRole($roleUser);
+        });
     }
 
     /**
